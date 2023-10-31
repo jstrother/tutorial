278e07bdcd54c87c70cfa0ce7508924c5926d6a4
Jim <jim.strother@gmail.com>
2023-10-31 11:32:19 -0500

    Fixed error in rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,12 +7,12 @@ export async function GET(context) {
     title: 'Jim Strother | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: posts.map((post) => {
+    items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}`,
-    }),
+    })),
     customData: `<language>en-us</language>`,
   });
 }
